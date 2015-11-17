fe3a6fe885fc0b945aa62ccb4f541d98248547de
Kate Lance <kate@6now.net>
2015-11-17 15:20:56 +1100

    Add files that appeared to be missing

---
+++ v2/shop.php
@@ -0,0 +1,43 @@
+<h1>
+  <img src="<?=VERSION?>/images/IPv6NowLogo.png"><br>
+  Get some gear. Your co-workers will be so jealous...<br>
+(Support us, so we can support you)
+</h1>
+
+<h4>Easy IPv6: The Lookup Book <i class="small">[Second edition.]</i><span>$24.95USD</span></h4>
+<img class="lookupbook" src="<?=VERSION?>/images/Lookup.png" alt="Easy IPv6: The Lookup Book">
+
+<div id="lookupbook-buynow">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="VK27QV9Z5PXZ8">
+<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_buynow_SM.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_AU/i/scr/pixel.gif" width="1" height="1">
+</form>
+</div>
+
+
+
+<p>
+The Lookup Book is unique: It is the only compact and precise, quick reference to
+IPv6 technical definitions available.
+</p>
+<p>
+It covers IPv6 addresses, prefixes, packet structure, address types,
+multicast, ICMPv6, autoconfiguration, DNS, transition, security, mobility, and
+tools. This expanded Second Edition also has new sections on tunnel techniques
+and IPv6 packet and routing security.
+</p>
+<p class="small">
+28pp, colour pocket-size paperback, spiral bound 105 x 148 mm, ISBN 978-0-9872113-0-9
+</p>
+
+
+<h4>T-Shirts are great</h4>
+<p>
+Okay we admit it, you're going to have to be a pretty special sort of person
+to wear a t-shirt that proclaims your IPv6 preference for the world to see...
+</p>
+<p>
+But that's ok. We think you're great. Don't forget to buy one for your mum.
+</p>
